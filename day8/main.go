a58e236a50614b86475e5779f17917d65e51001b
Zhangyq <sharpe@ZhangYinQian>
2022-08-14 00:03:33 +0800

    day 8

---
+++ day8/main.go
@@ -0,0 +1,35 @@
+//https://leetcode.cn/problems/reverse-linked-list/
+
+package main
+
+func main() {
+
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+
+	var pre *ListNode
+	cur := head
+
+	for cur != nil {
+		next := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = next
+	}
+
+	return pre
+}
