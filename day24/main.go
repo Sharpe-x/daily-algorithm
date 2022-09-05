fa999ee4d6488ea185f8adb56beb52f9323be1d6
sharpex <zhangyq008@gmail.com>
2022-09-05 23:14:21 +0800

    day 25

--- day24/main.go
+++ day24/main.go
@@ -3,31 +3,31 @@ package main
 import "container/list"
 
 // 队列 非并发安全
-type MyQueue struct {
+type MyQueue_List struct {
 	list *list.List
 }
 
-func Constructor() MyQueue {
-	return MyQueue{
+func Constructor() MyQueue_List {
+	return MyQueue_List{
 		list: list.New(),
 	}
 }
 
-func (this *MyQueue) Push(x int) {
+func (this *MyQueue_List) Push(x int) {
 	this.list.PushBack(x)
 }
 
-func (this *MyQueue) Pop() int {
+func (this *MyQueue_List) Pop() int {
 	e := this.list.Front()
 	num := e.Value.(int)
 	this.list.Remove(e)
 	return num
 }
 
-func (this *MyQueue) Peek() int {
+func (this *MyQueue_List) Peek() int {
 	return this.list.Front().Value.(int)
 }
 
-func (this *MyQueue) Empty() bool {
+func (this *MyQueue_List) Empty() bool {
 	return this.list.Len() == 0
 }
--- week2-review/main.go
+++ week2-review/main.go
@@ -325,3 +325,27 @@ func getIntersectionNode(headA, headB *ListNode) *ListNode {
 	return nil
 
 }
+
+func detectCycle(head *ListNode) *ListNode {
+
+	fast, slow := head, head
+
+	for fast != nil && fast.Next != nil {
+
+		fast = fast.Next.Next
+		slow = slow.Next
+
+		if fast == slow {
+			slow = head
+			for slow != fast {
+				slow = slow.Next
+				fast = fast.Next
+			}
+
+			return slow
+		}
+
+	}
+
+	return nil
+}
