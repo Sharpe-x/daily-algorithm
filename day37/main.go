96eb1186884bd76dfb87a858cd35a8aa6f0070b7
sharpex <zhangyq008@gmail.com>
2022-10-08 23:31:13 +0800

    day37

--- day37/main.go
+++ day37/main.go
@@ -23,3 +23,32 @@ func getNodeNum(node *TreeNode) int {
 
 	return left + right + 1
 }
+
+// 利用完全二叉树的特性
+func getNodeNum2(node *TreeNode) int {
+	if node == nil {
+		return 0
+	}
+
+	left := node.Left
+	right := node.Right
+
+	leftHight, rightHight := 0, 0
+	for left != nil {
+		left = left.Left
+		leftHight++
+	}
+
+	for right != nil {
+		right = right.Right
+		rightHight++
+	}
+
+	if leftHight == rightHight {
+		return 2<<leftHight - 1
+	}
+
+	leftNum := getNodeNum2(node.Left)
+	righNum := getNodeNum2(node.Right)
+	return leftNum + righNum + 1
+}
